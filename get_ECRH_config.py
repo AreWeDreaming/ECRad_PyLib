00e1f7cae1b7b70205f0d6ba00f52fe9f63421b4
Severin Denk <severin.denk@ipp.mpg.de>
2019-05-23 13:52:57 +0200

    ITM commit

--- get_ECRH_config.py
+++ get_ECRH_config.py
@@ -190,6 +190,7 @@ class gyrotron(AUGLauncher):
         # Angles are given in degrees
         libECRH_obj = libECRH_wrapper(shot)
         self.error = 0
+        self.N =N
         if(N > 4):
             N_2 = N - 4
         else:
