d173f4a9f0bddd7ef150397bcba176abecc7fc79
Severin Denk <denks@fusion.gat.com>
2024-11-06 17:02:30 -0800

    Fix batch submisison for omega

--- src/ecrad_pylib/Global_Settings.py
+++ src/ecrad_pylib/Global_Settings.py
@@ -32,6 +32,9 @@ def qos_function_itm(cores, wall_time):
 def qos_function_iris(cores, wall_time):
     return ""
 
+def qos_function_omega(cores, wall_time):
+    return ""
+
 def partition_function_tok(cores, wall_time):
     if(cores == 1):
         #serial
@@ -50,6 +53,15 @@ def partition_function_iris(cores, wall_time):
     else:
         return "--partition=long"
 
+def partition_function_omega(cores, wall_time):
+    if(wall_time <= 0.5):
+        return "--partition=short"
+    elif(wall_time > 0.5 and wall_time <= 24):
+        return "--partition=medium"
+    else:
+        return "--partition=long"
+
+
 def partition_function_engaging(cores, wall_time):
     return "--partition=sched_mit_psfc"
 
@@ -63,22 +75,27 @@ class GlobalSettings:
     def __init__(self):
         self.AUG = False  # True  -> Start with True, set it to false if we run into problems
         self.root = os.path.expanduser("~/")
-        self.ECRadRoot = os.path.join(os.path.dirname(__file__), os.pardir)
-        self.ECRadCoreRoot = os.path.join(self.ECRadRoot, "ECRad_core")
-        self.ECRadPylibRoot = os.path.join(self.ECRadRoot, "ECRad_PyLib")
-        self.ECRadGUIRoot = os.path.join(self.ECRadRoot, "ECRad_GUI")
+        self.ECRadRoot = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir)
+        self.ECRadCoreRoot = os.path.join(self.ECRadRoot, "ECRad_core/src/ecrad_core")
+        self.ECRadPylibRoot = os.path.join(self.ECRadRoot, "ECRad_PyLib/src/ecrad_pylib")
+        self.ECRadGUIRoot = os.path.join(self.ECRadRoot, "ECRad_GUI/src/ecrad_gui")
         if "SYS" in os.environ:
             self.ECRadLibDir = os.path.join(self.ECRadRoot, os.environ["SYS"])
         else:
             self.ECRadLibDir = os.path.join(self.ECRadRoot, "bin")
         self.ECRadPathBSUB = os.path.join(self.ECRadPylibRoot,"ECRad_Driver_submit.bsub")
         self.TB_path = os.path.abspath("../libtorbeam")
-        self.batch_submission_cmd = "bash"
-        self.qos_function = qos_function_tok
-        self.partition_function = partition_function_tok
-        self.account_fuction = account_function_current_user
+        self.batch_submission_cmd = "sbatch"
+        if "omega" in os.environ["HOSTNAME"]:
+            self.qos_function = qos_function_omega
+            self.partition_function = partition_function_omega
+            self.account_fuction = account_function_current_user
+        else:
+            self.qos_function = qos_function_tok
+            self.partition_function = partition_function_tok
+            self.account_fuction = account_function_current_user
         self.max_cores = multiprocessing.cpu_count() / 2
-        self.plot_mode = "Presentation"
+        self.plot_mode = "Software"
         self.mathrm = r"\mathrm"
         self.omas = True
         try:
--- src/ecrad_pylib/run_ECRad_Driver.sh
+++ src/ecrad_pylib/run_ECRad_Driver.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source ../ECRad_core/set_environment.sh
 if [ $# == 2 ]
   then
   python ECRad_Driver.py $1 $2
